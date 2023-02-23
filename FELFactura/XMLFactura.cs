2166362ae3f84572c3d776e170e6db1c47aac885
hectorcuevas <hcuevas@prosisco.com.gt>
2023-02-22 18:57:05 -0600

    cambios NCRE

--- FELFactura/XMLFactura.cs
+++ FELFactura/XMLFactura.cs
@@ -378,6 +378,7 @@ namespace FELFactura
                         {
                             XElement Referencias = new XElement(cno + "ReferenciasNota"
                                , new XAttribute(XNamespace.Xmlns + "cno", cno)
+                               , new XAttribute("NumeroDocumentoOrigen", nota.NumeroDocumentoOrigen)
                                , new XAttribute("FechaEmisionDocumentoOrigen", nota.FechaEmisionDocumentoOrigen)
                                , new XAttribute("MotivoAjuste", nota.MotivoAjuste)
                                , new XAttribute("NumeroAutorizacionDocumentoOrigen", nota.NumeroAutorizacionDocumentoOrigen)
@@ -427,6 +428,7 @@ namespace FELFactura
 
                         XElement Referencias = new XElement(cno + "ReferenciasNota"
                             , new XAttribute(XNamespace.Xmlns + "cno", cno)
+                            , new XAttribute("NumeroDocumentoOrigen", nota.NumeroDocumentoOrigen)
                             , new XAttribute("FechaEmisionDocumentoOrigen", nota.FechaEmisionDocumentoOrigen)
                             , new XAttribute("MotivoAjuste", nota.MotivoAjuste)
                             , new XAttribute("NumeroAutorizacionDocumentoOrigen", nota.NumeroAutorizacionDocumentoOrigen)
