159880e902f72024ebe88a3b5e4bcdb5a8b8d16c
hectorcuevas <hcuevas@prosisco.com.gt>
2023-07-25 13:32:12 -0600

    cambio tipoespecial

--- FELFactura/XMLFactura.cs
+++ FELFactura/XMLFactura.cs
@@ -370,6 +370,7 @@ namespace FELFactura
                         {
                             XElement Referencias = new XElement(cno + "ReferenciasNota"
                                , new XAttribute(XNamespace.Xmlns + "cno", cno)
+                           , new XAttribute("NumeroDocumentoOrigen", nota.NumeroDocumentoOrigen)
                                , new XAttribute("FechaEmisionDocumentoOrigen", nota.FechaEmisionDocumentoOrigen)
                                , new XAttribute("MotivoAjuste", nota.MotivoAjuste)
                                , new XAttribute("NumeroAutorizacionDocumentoOrigen", nota.NumeroAutorizacionDocumentoOrigen)
@@ -419,6 +420,7 @@ namespace FELFactura
 
                         XElement Referencias = new XElement(cno + "ReferenciasNota"
                             , new XAttribute(XNamespace.Xmlns + "cno", cno)
+                            , new XAttribute("NumeroDocumentoOrigen", nota.NumeroDocumentoOrigen)
                             , new XAttribute("FechaEmisionDocumentoOrigen", nota.FechaEmisionDocumentoOrigen)
                             , new XAttribute("MotivoAjuste", nota.MotivoAjuste)
                             , new XAttribute("NumeroAutorizacionDocumentoOrigen", nota.NumeroAutorizacionDocumentoOrigen)
